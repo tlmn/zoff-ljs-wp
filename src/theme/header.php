2ef0b41fd439c38974144d0ecf19abb1386d3f1b
Tilman Miraß <mail@tlmn.io>
2021-05-14 19:38:41 +0200

    add skeleton markup for mobile menu

--- src/theme/header.php
+++ src/theme/header.php
@@ -16,3 +16,27 @@
 </head>
 
 <body class="<?php print join(' ', get_body_class()); ?>'">
+
+	<div class="hidden md:block bg-black">
+		<div class="">
+
+		</div>
+	</div>
+
+	<div class="flex items-end flex-col justify-end md:hidden fixed top-0 left-0 h-screen h-max-screen w-screen w-max-screen" id="mobile-menu">
+		<div class="hidden flex-1" id="mobile-navigation">
+			Menu
+		</div>
+		<div class="mr-2 mb-2">
+			<button role="button" class="" onclick="toggleMenu()" id="button__menu--open">
+				<?php
+				echo file_get_contents(get_template_directory() . '/assets/images/icons/hamburger--open.svg');
+				?>
+			</button>
+			<button role="button" class="hidden" onclick="toggleMenu()" id="button__menu--close">
+				<?php
+				echo file_get_contents(get_template_directory() . '/assets/images/icons/hamburger--close.svg');
+				?>
+			</button>
+		</div>
+	</div>

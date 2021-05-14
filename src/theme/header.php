fdbe57b84b10fd5f649c469278bbe090e4bd3bb6
Tilman Miraß <mail@tlmn.io>
2021-05-14 20:35:00 +0200

    add menus

--- src/theme/header.php
+++ src/theme/header.php
@@ -18,14 +18,87 @@
 <body class="<?php print join(' ', get_body_class()); ?>'">
 
 	<div class="hidden md:block bg-black">
-		<div class="">
+		<div class="container flex items-center">
+			<div class="w-full flex justify-start">
+				<a href="/" style="height: 1.5rem; width: 1.5rem;">
+					<?php
+					echo file_get_contents(get_template_directory() . '/assets/images/icons/ljs-logo--white.svg');
+					?>
+				</a>
+			</div>
+			<?php
 
+			wp_nav_menu(array(
+				'menu'                 => 'primary_menu',
+				'container'            => 'div',
+				'container_class'      => '',
+				'menu_class'           => 'menu primary_menu',
+				'echo'                 => true,
+				'fallback_cb'          => 'wp_page_menu',
+				'theme_location'       => 'primary_menu',
+			));
+
+			?>
+			<div class="w-full flex justify-center">
+				<a href="/suche" style="height: 1.5rem; width: 1.5rem;">
+					<?php
+					echo file_get_contents(get_template_directory() . '/assets/images/icons/lens.svg');
+					?>
+				</a>
+			</div>
+			<?php
+
+			wp_nav_menu(array(
+				'menu'                 => 'top_menu',
+				'container'            => 'div',
+				'container_class'      => '',
+				'menu_class'           => 'menu top_menu',
+				'echo'                 => true,
+				'fallback_cb'          => 'wp_page_menu',
+				'theme_location'       => 'top_menu',
+			));
+
+			?>
 		</div>
 	</div>
 
 	<div class="flex items-end flex-col justify-end md:hidden fixed top-0 left-0 h-screen h-max-screen w-screen w-max-screen" id="mobile-menu">
-		<div class="hidden flex-1" id="mobile-navigation">
-			Menu
+		<div class="hidden flex-1 w-full" id="mobile-navigation">
+			<div class="w-full py-5 flex justify-center">
+				<a href="/" style="height: 2.5rem; width: 2.5rem;">
+					<?php
+					echo file_get_contents(get_template_directory() . '/assets/images/icons/ljs-logo--white.svg');
+					?>
+				</a>
+			</div>
+			<?php
+			wp_nav_menu(array(
+				'menu'                 => 'top_menu',
+				'container'            => 'div',
+				'container_class'      => 'w-full flex flex-col',
+				'menu_class'           => 'menu top_menu',
+				'echo'                 => true,
+				'fallback_cb'          => 'wp_page_menu',
+				'theme_location'       => 'top_menu',
+			));
+
+			wp_nav_menu(array(
+				'menu'                 => 'primary_menu',
+				'container'            => 'div',
+				'container_class'      => 'w-full flex flex-col',
+				'menu_class'           => 'menu primary_menu',
+				'echo'                 => true,
+				'fallback_cb'          => 'wp_page_menu',
+				'theme_location'       => 'primary_menu',
+			));
+			?>
+			<div class="w-full py-5 flex justify-center">
+				<a href="/suche" style="height: 2.5rem; width: 2.5rem;">
+					<?php
+					echo file_get_contents(get_template_directory() . '/assets/images/icons/lens.svg');
+					?>
+				</a>
+			</div>
 		</div>
 		<div class="mr-2 mb-2">
 			<button role="button" class="" onclick="toggleMenu()" id="button__menu--open">

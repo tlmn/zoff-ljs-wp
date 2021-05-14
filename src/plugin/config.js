59738e81f000e098af1d5be166e8d694096fbbbf
Tilman Miraß <mail@tlmn.io>
2021-05-14 14:46:47 +0200

    add color config

---
+++ src/plugin/config.js
@@ -0,0 +1,70 @@
+export const colors = [
+  { name: "black", value: "#252525" },
+  { name: "red", value: "#E2190C" },
+  { name: "green", value: "#00FFC2" },
+  { name: "purple", value: "#C9CEFF" },
+  { name: "white", value: "#fff" },
+];
+
+export const primaryColors = [
+  { label: "green", name: "grün" },
+  { label: "purple", name: "purple" },
+  { label: "red", name: "rot" },
+];
+
+export const corporateThemes = [
+  "black_green",
+  "green_black",
+  "purple_red",
+  "red_purple",
+];
+
+export const secondaryThemes = [
+  "black_red",
+  "red_black",
+  "red_white",
+  "white_red",
+];
+
+export const colorThemes = [
+  {
+    label: "black_green",
+    name: "schwarz & grün",
+    colors: ["black", "green"],
+  },
+  {
+    label: "green_black",
+    name: "grün & schwarz",
+    colors: ["green", "black"],
+  },
+  {
+    label: "purple_red",
+    name: "lila & rot",
+    colors: ["purple", "red"],
+  },
+  {
+    label: "red_purple",
+    name: "rot & lila",
+    colors: ["red", "purple"],
+  },
+  {
+    label: "black_red",
+    name: "schwarz & rot",
+    colors: ["black", "red"],
+  },
+  {
+    label: "red_black",
+    name: "rot & schwarz",
+    colors: ["red", "black"],
+  },
+  {
+    label: "red_white",
+    name: "rot & weiß",
+    colors: ["red", "white"],
+  },
+  {
+    label: "white_red",
+    name: "weiß & rot",
+    colors: ["white", "red"],
+  },
+];

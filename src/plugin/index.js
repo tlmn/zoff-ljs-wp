9e2b71f4270f5daffe6f2049c6f1e213202bc2fe
Tilman Miraß <mail@tlmn.io>
2021-05-16 21:19:37 +0200

    add quote block

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -5,6 +5,7 @@ import "./blocks/intro-text";
 import "./blocks/tiles/container";
 import "./blocks/tiles/single";
 import "./blocks/breaker";
+import "./blocks/quote";
 import "./blocks/contentTeaser/container";
 import "./blocks/contentTeaser/columnTitle";
 import "./blocks/contentTeaser/columnBody";

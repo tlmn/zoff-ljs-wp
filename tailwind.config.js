ed2dd6f9aeff48c9cdb456d9326114823cc67bd6
Tilman Miraß <mail@tlmn.io>
2021-05-17 16:30:01 +0200

    add borderWidth to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
   //   },
   // },
   theme: {
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+    },
     fontFamily: {
       gilroy: ["Gilroy", "Helvetica", "Arial", "sans-serif"],
       hind: ["Hind", "Helvetica", "Arial", "sans-serif"],

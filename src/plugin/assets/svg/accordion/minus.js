d6331cc315880af35cc38ee110034999085a6496
Tilman Miraß <mail@tlmn.io>
2021-05-21 11:45:53 +0200

    add plus and minus icons

---
+++ src/plugin/assets/svg/accordion/minus.js
@@ -0,0 +1,12 @@
+export default ({ fillColor = "#252525" }) => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <rect
+      x="23.7383"
+      y="10.96"
+      width="2.7011"
+      height="22.8552"
+      transform="rotate(90 23.7383 10.96)"
+      fill={fillColor}
+    />
+  </svg>
+);
---
+++ src/plugin/assets/svg/accordion/plus.js
@@ -0,0 +1,19 @@
+export default ({ fillColor = "#252525" }) => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <rect
+      x="10.9609"
+      y="0.556152"
+      width="2.7011"
+      height="22.8552"
+      fill={fillColor}
+    />
+    <rect
+      x="23.7383"
+      y="10.6328"
+      width="2.7011"
+      height="22.8552"
+      transform="rotate(90 23.7383 10.6328)"
+      fill={fillColor}
+    />
+  </svg>
+);

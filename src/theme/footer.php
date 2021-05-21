6e6f1c50456db51c10405cbf607a70973c1f85df
Tilman Miraß <mail@tlmn.io>
2021-05-21 13:59:00 +0200

    add footer

--- src/theme/footer.php
+++ src/theme/footer.php
@@ -1,4 +1,18 @@
-<footer></footer>
+<footer>
+    <div class="container">
+        <?php
+
+        wp_nav_menu(array(
+            'menu'                 => 'footer',
+            'menu_class'           => 'menu footer_menu',
+            'echo'                 => true,
+            'fallback_cb'          => 'wp_page_menu',
+            'theme_location'       => 'footer_menu',
+        ));
+
+        ?>
+    </div>
+</footer>
 <?php wp_footer(); ?>
 </body>
 

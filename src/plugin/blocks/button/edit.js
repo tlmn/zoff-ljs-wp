76b2e6c63eae82ae638f453ffcc04195a078d29f
Tilman Miraß <mail@tlmn.io>
2021-05-21 18:20:27 +0200

    add useBlockProps

--- src/plugin/blocks/button/edit.js
+++ src/plugin/blocks/button/edit.js
@@ -2,12 +2,18 @@ import { getPrimaryColorName, getSecondaryColorName } from "../../lib/lib";
 
 import URLPicker from "../../inspector/URLPicker";
 
-const { RichText, InspectorControls } = window.wp.blockEditor;
+const { RichText, InspectorControls, useBlockProps } = window.wp.blockEditor;
 
 const { __ } = window.wp.i18n;
 
 export default (props) => {
   const { attributes, setAttributes } = props;
+
+  const blockProps = useBlockProps({
+    className: `ljs-button text-${getSecondaryColorName(
+      attributes.colorTheme
+    )} bg-${getPrimaryColorName(attributes.colorTheme)}`,
+  });
 
   return (
     <>
@@ -15,11 +21,7 @@ export default (props) => {
         <URLPicker {...props} />
       </InspectorControls>
 
-      <div
-        className={`ljs-button text-${getSecondaryColorName(
-          attributes.colorTheme
-        )} bg-${getPrimaryColorName(attributes.colorTheme)}`}
-      >
+      <div {...blockProps}>
         <RichText
           value={attributes.buttonText}
           allowedFormats={[]}
--- src/plugin/blocks/button/save.js
+++ src/plugin/blocks/button/save.js
@@ -1,23 +1,25 @@
 import { getPrimaryColorName, getSecondaryColorName } from "../../lib/lib";
 
 const { __ } = window.wp.i18n;
+const { useBlockProps } = window.wp.blockEditor;
 
 export default (props) => {
   const { attributes } = props;
 
+  const blockProps = useBlockProps.save({
+    className: `ljs-button text-${getSecondaryColorName(
+      attributes.colorTheme
+    )} bg-${getPrimaryColorName(
+      attributes.colorTheme
+    )} hover:text-${getPrimaryColorName(
+      attributes.colorTheme
+    )} hover:bg-${getSecondaryColorName(
+      attributes.colorTheme
+    )} hover:border-${getPrimaryColorName(attributes.colorTheme)}`,
+  });
+
   return (
-    <a
-      className={`ljs-button text-${getSecondaryColorName(
-        attributes.colorTheme
-      )} bg-${getPrimaryColorName(
-        attributes.colorTheme
-      )} hover:text-${getPrimaryColorName(
-        attributes.colorTheme
-      )} hover:bg-${getSecondaryColorName(
-        attributes.colorTheme
-      )} hover:border-${getPrimaryColorName(attributes.colorTheme)}`}
-      href={attributes.url}
-    >
+    <a href={attributes.url} {...blockProps}>
       {attributes.buttonText}
     </a>
   );

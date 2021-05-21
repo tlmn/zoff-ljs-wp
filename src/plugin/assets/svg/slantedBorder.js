aea0074f086db16b0d4a1e4a5ec804e8df98e75f
Tilman Miraß <mail@tlmn.io>
2021-05-21 12:13:41 +0200

    use classes instead of ids

--- src/plugin/assets/svg/slantedBorder.js
+++ src/plugin/assets/svg/slantedBorder.js
@@ -3,8 +3,9 @@ export default ({ fillColor, flipped = false }) => (
     width="100%"
     viewBox="0 0 500 10"
     style={{ transform: flipped ? `rotate(180deg)` : `` }}
-    className="relative z-20"
-    id={flipped ? `slantedBorder` : `slantedBorder--flipped`}
+    className={`relative z-20 ${
+      flipped ? `slantedBorder` : `slantedBorder--flipped`
+    }`}
   >
     <polygon points="0,10 500,10 500,0" fill={fillColor} />
   </svg>

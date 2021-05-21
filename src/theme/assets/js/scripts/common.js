70ff9b851947498f987bf64aa55b112844b9c028
Tilman Miraß <mail@tlmn.io>
2021-05-21 11:46:17 +0200

    add functions for accordion functionality

--- src/theme/assets/js/scripts/common.js
+++ src/theme/assets/js/scripts/common.js
@@ -7,3 +7,19 @@ const toggleMenu = () => {
   $("#button__menu--close").toggleClass("hidden");
   $("body").toggleClass("overflow-hidden");
 };
+
+const handleDetailsButtonClick = ({ currentTarget }) => {
+  var details = $(currentTarget).parents().eq(2).find("details");
+  if (typeof details.attr("open") !== typeof undefined) {
+    details.removeAttr("open");
+    toggleButtons(currentTarget);
+  } else {
+    details.attr("open", "");
+    toggleButtons(currentTarget);
+  }
+};
+
+const toggleButtons = (currentTarget) => {
+  $(currentTarget).toggleClass("hidden");
+  $(currentTarget).siblings().toggleClass("hidden");
+};

9d1e924624cdb0c3110fb8f686d5dbd6caf20805
Tilman Miraß <mail@tlmn.io>
2021-05-23 11:50:40 +0200

    remove site.manifest

--- src/theme/header.php
+++ src/theme/header.php
@@ -10,7 +10,6 @@
 	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . "/manifest/"; ?>apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() . "/manifest/"; ?>favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() . "/manifest/"; ?>favicon-16x16.png">
-	<link rel="manifest" href="<?php echo get_template_directory_uri() . "/manifest/"; ?>site.webmanifest">
 	<link rel="mask-icon" href="<?php echo get_template_directory_uri() . "/manifest/"; ?>safari-pinned-tab.svg" color="#5bbad5">
 	<?php wp_head(); ?>
 </head>

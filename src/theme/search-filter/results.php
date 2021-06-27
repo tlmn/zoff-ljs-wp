555079c6880974b87c448aa9e9eb1025ad0c7215
Tilman Miraß <mail@tlmn.io>
2021-06-27 14:26:31 +0200

    add results page

---
+++ src/theme/search-filter/results.php
@@ -0,0 +1,69 @@
+<?php
+
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+if ($query->have_posts()) {
+	$currentPage = $query->query['paged'];
+	$maxPage = $query->max_num_pages;
+?>
+
+	Seite <?php echo $currentPage; ?> von <?php echo $maxPage; ?><br />
+
+	<div class="pagination">
+		<?php
+		if ((int)$currentPage !== 1) {
+		?>
+			<a href="?sf_paged=<?php echo $currentPage - 1; ?>" class="wp-block-ljs-button bg-green text-black">Zurück</a>
+		<?php
+		}
+
+		if ((int)$currentPage !== (int)$maxPage && (int)$maxPage !== 1) {
+		?>
+			<a href="?sf_paged=<?php echo $currentPage + 1; ?>" class="wp-block-ljs-button bg-green text-black">Weiter</a>
+		<?php
+		}
+		?>
+	</div>
+
+	<?php
+	while ($query->have_posts()) {
+		$query->the_post();
+
+	?>
+		<div class="my-5">
+			<h2 class="mb-3">
+				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+			</h2>
+
+			<p>
+				<?php the_excerpt(); ?>
+			</p>
+		</div>
+
+	<?php
+	}
+	?>
+	Seite <?php echo $currentPage; ?> von <?php echo $maxPage; ?><br />
+
+	<div class="pagination">
+		<?php
+		if ((int)$currentPage !== 1) {
+		?>
+			<a href="?sf_paged=<?php echo $currentPage - 1; ?>" class="wp-block-ljs-button bg-green text-black">Zurück</a>
+		<?php
+		}
+
+		if ((int)$currentPage !== (int)$maxPage && (int)$maxPage !== 1) {
+		?>
+			<a href="?sf_paged=<?php echo $currentPage + 1; ?>" class="wp-block-ljs-button bg-green text-black">Weiter</a>
+		<?php
+		}
+		?>
+	</div>
+<?php
+} else {
+	echo "No Results Found";
+}
+?>

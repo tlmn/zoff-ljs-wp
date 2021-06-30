04ade2db20291ef6e01f2901b95d00435b43a61b
Tilman Miraß <mail@tlmn.io>
2021-06-30 16:07:05 +0200

    remove pagination top

--- src/theme/search-filter/results.php
+++ src/theme/search-filter/results.php
@@ -7,31 +7,10 @@ if (!defined('ABSPATH')) {
 if ($query->have_posts()) {
 	$currentPage = $query->query['paged'];
 	$maxPage = $query->max_num_pages;
-?>
-
-	Seite <?php echo $currentPage; ?> von <?php echo $maxPage; ?><br />
-
-	<div class="pagination">
-		<?php
-		if ((int)$currentPage !== 1) {
-		?>
-			<a href="?sf_paged=<?php echo $currentPage - 1; ?>" class="wp-block-ljs-button bg-green text-black">Zurück</a>
-		<?php
-		}
-
-		if ((int)$currentPage !== (int)$maxPage && (int)$maxPage !== 1) {
-		?>
-			<a href="?sf_paged=<?php echo $currentPage + 1; ?>" class="wp-block-ljs-button bg-green text-black">Weiter</a>
-		<?php
-		}
-		?>
-	</div>
-
-	<?php
 	while ($query->have_posts()) {
 		$query->the_post();
 
-	?>
+?>
 		<div class="my-5">
 			<h2 class="mb-3">
 				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
@@ -60,7 +39,7 @@ if ($query->have_posts()) {
 
 	<?php
 	}
-	
+
 	?>
 	Seite <?php echo $currentPage; ?> von <?php echo $maxPage; ?><br />
 

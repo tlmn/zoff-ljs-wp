eb042b1d703034908ff9926d0a580400fedabb35
Tilman Miraß <mail@tlmn.io>
2021-07-06 21:50:10 +0200

    add LJS to title

--- src/theme/header.php
+++ src/theme/header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title><?php wp_title('&ndash;', true, 'right'); ?></title>
+	<title><?php wp_title('&ndash;', true, 'right'); ?> Linksjugend ['solid]</title>
 	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . "/manifest/"; ?>apple-touch-icon.png">

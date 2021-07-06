72693e9f3e64947642c4c1f71aca4b40fec007f3
Tilman Miraß <mail@tlmn.io>
2021-07-06 21:24:44 +0200

    create 404 page

--- src/theme/404.php
+++ src/theme/404.php
@@ -1,10 +1,14 @@
 <?php
 get_header();
 ?>
-<article>
-	<header>
-		<h1><?php echo '404. Page Not Found.'; ?></h1>
-	</header>
-</article>
+<div class="container grid-12 py-8">
+	<div class="col-span-full md:col-start-3 md:col-span-8">
+		<h1 class="text-center">
+			😭
+			<br />
+			Die angeforderte Seite gibt's leider nicht.
+		</h1>
+	</div>
+</div>
 <?php
 get_footer();

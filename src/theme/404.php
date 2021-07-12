0c2dec1ccce95ade95a8cac06dbac56e65d459f9
Tilman Miraß <mail@tlmn.io>
2021-07-12 11:59:51 +0200

    remove italic on emoji

--- src/theme/404.php
+++ src/theme/404.php
@@ -4,7 +4,7 @@ get_header();
 <div class="container grid-12 py-8">
 	<div class="col-span-full md:col-start-3 md:col-span-8">
 		<h1 class="text-center">
-			😭
+			<span class="not-italic">😭</span>
 			<br />
 			Die angeforderte Seite gibt's leider nicht.
 		</h1>

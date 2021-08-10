bb8732d22b1b0867f733fa4bcccd80e8a5aa87dd
tlmn <mail@tlmn.io>
2021-08-10 07:46:54 +0200

    add missing icons to save function

--- src/plugin/blocks/contactBar/item/save.js
+++ src/plugin/blocks/contactBar/item/save.js
@@ -3,7 +3,10 @@ const { useBlockProps } = wp.blockEditor;
 import FacebookIcon from "../../../assets/svg/contactItems/facebook";
 import InstagramIcon from "../../../assets/svg/contactItems/instagram";
 import MailIcon from "../../../assets/svg/contactItems/mail";
+import TelegramIcon from "../../../assets/svg/contactItems/telegram";
+import TikTokIcon from "../../../assets/svg/contactItems/tiktok";
 import TwitterIcon from "../../../assets/svg/contactItems/twitter";
+import WebsiteIcon from "../../../assets/svg/contactItems/website";
 import { getPrimaryColorName } from "../../../lib/lib";
 
 export default ({ attributes: { colorTheme, contactType, url } }) => {
@@ -27,6 +30,21 @@ export default ({ attributes: { colorTheme, contactType, url } }) => {
       name: "Instagram",
       value: "instagram",
       component: <InstagramIcon />,
+    },
+    {
+      name: "Website",
+      value: "website",
+      component: <WebsiteIcon />,
+    },
+    {
+      name: "Telegram",
+      value: "telegram",
+      component: <TelegramIcon />,
+    },
+    {
+      name: "TikTok",
+      value: "tiktok",
+      component: <TikTokIcon />,
     },
   ];
 

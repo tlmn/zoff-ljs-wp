dd56fc640e835275a4331db60837986122e3ebfb
tlmn <mail@tlmn.io>
2021-08-19 17:28:25 +0200

    remove unneeded import

--- src/plugin/blockComponents/image/image.js
+++ src/plugin/blockComponents/image/image.js
@@ -1,12 +1,10 @@
 import imageCrowd from "../../assets/images/placeholders/crowd.jpg";
 import imagePersonFemale from "../../assets/images/placeholders/personFemale.jpg";
-import imagePersonMale from "../../assets/images/placeholders/personMale.jpg";
 
 export default ({ attributes, className, placeholder = "crowd" }) => {
   const placeholderImages = {
     crowd: imageCrowd,
     personFemale: imagePersonFemale,
-    personMale: imagePersonMale,
   };
 
   const { mediaId, mediaSrcSet } = attributes;

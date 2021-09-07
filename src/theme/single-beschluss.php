715e86687e497c3bf4b409309a3fdc81e9920611
tlmn <mail@tlmn.io>
2021-09-07 10:35:52 +0200

    add my-5

--- src/theme/single-beschluss.php
+++ src/theme/single-beschluss.php
@@ -4,7 +4,7 @@ get_header();
 if (have_posts()) :
 	while (have_posts()) : the_post();
 ?>
-		<div class="container grid-6 md:grid-12">
+		<div class="container grid-6 md:grid-12 my-6">
 			<div class="col-span-full">
 				<?php
 				the_content();

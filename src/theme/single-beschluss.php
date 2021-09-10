bfa31b3b62dfcefcd54a916e89a1efddfc1e30cd
tlmn <mail@tlmn.io>
2021-09-10 13:47:19 +0200

    add id and col-span

--- src/theme/single-beschluss.php
+++ src/theme/single-beschluss.php
@@ -1,18 +1,16 @@
 <?php
 get_header();
 
-if (have_posts()) :
-	while (have_posts()) : the_post();
-?>
-		<div class="container grid-6 md:grid-12 my-6">
-			<div class="col-span-full">
-				<?php
-				the_content();
-				?>
+if (have_posts()):
+    while (have_posts()):
+        the_post(); ?>
+		<div class="container grid-6 md:grid-12 my-6" id="single-beschluss">
+			<div class="col-span-full lg:col-span-9 lg:col-start-2">
+				<?php the_content(); ?>
 			</div>
 		</div>
 <?php
-	endwhile;
+    endwhile;
 endif;
 
 get_footer();
